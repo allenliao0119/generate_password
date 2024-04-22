645f624d8007fff3a7a17deaeb7d105dc0f54392
AllenLiao <allen10153@gmail.com>
2024-04-22 23:25:27 +0800

    feat: add express-handlebars and index page

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 // include modules from npm
 const express = require('express')
+const exphbs = require('express-handlebars')
 
 const app = express()
+
+// set view template
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
 
 // define the relative variables
 const port = 3000
 
 // set the routes
 app.get('/', (req, res) => {
-  res.send('This will be a ramdon password generator.')
+  res.render('index')
 })
 
 // start and listen the server
